3fef770c77cad2948fad3433a23e466b778f2185
Luana Marina <lmsf1@aluno.ifal.edu.br>
2022-09-12 17:44:24 +0000

    Adcionando Subtracao

---
+++ api/src/main/java/app/Subtracao.java
@@ -0,0 +1,24 @@
+package app;
+
+import io.jooby.annotations.*;
+import io.jooby.exception.BadRequestException;
+
+@Path("/subtracao/{op}/{op2}")
+public class Subtracao {
+    
+    @GET
+    public double calcula(@PathParam("op") String opStr, @PathParam("op2") String op2Str) {
+        try {
+            double op = Double.parseDouble(opStr);
+            double op2 = Double.parseDouble(op2Str);
+            if (op < 0) {
+                throw new BadRequestException("A calculadora só opera nos reais. Não funciono com números imaginários.");    
+            }
+            double a = op - op2;
+            return a;
+        } catch (NumberFormatException nfe) {
+            throw new BadRequestException("Operador inválido.");
+        }
+    }
+}
+
