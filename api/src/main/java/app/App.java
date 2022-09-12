7403c62d009c42f51500ea7e5e97c481d912df8b
Viviam Beatriz <vbgs1@aluno.ifal.edu.br>
2022-09-12 18:18:31 +0000

    .

--- api/src/main/java/app/App.java
+++ api/src/main/java/app/App.java
@@ -7,6 +7,9 @@ public class App extends Jooby {
   {
     mvc(new Index());
     mvc(new Adicao());
+    mvc(new Subtracao());
+    mvc(new Multiplicacao);
+    mvc(new Divisao());
   }
 
   public static void main(final String[] args) {
