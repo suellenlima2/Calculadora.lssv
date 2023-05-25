f99eb490a6d2a08d87ca322b7ee9bfb48da91f8d
Suellen Lima <sls24@aluno.ifal.edu.br>
2023-05-25 14:52:05 -0300

    Update Divisao.java

--- api/src/main/java/app/Divisao.java
+++ api/src/main/java/app/Divisao.java
@@ -5,22 +5,23 @@ import io.jooby.exception.BadRequestException;
 
 @Path("/divisao/{op1}/{op2}")
 
+//DIVISION
 public class Divisao {
     
     @GET
-    public double calcula(@PathParam("op1") String op1Str, @PathParam("op2") String op2Str) {
+    public double calculate(@PathParam("op1") String op1Str, @PathParam("op2") String op2Str) {
         try {
             double op1 = Double.parseDouble(op1Str);
             double op2 = Double.parseDouble(op2Str);
-            double resultado = 0;
+            double result = 0;
             if (op2 == 0) {
                 throw new BadRequestException("A calculadora só realizará divisões por 0.");    
             } else{
-                 resultado = op1 / op2 ; 
+                 result = op1 / op2 ; 
             }
-            return resultado;
+            return result;
         } catch (NumberFormatException nfe) {
-            throw new BadRequestException(" O operador é inválido.");
+            throw new BadRequestException("operator is not valid");
         }
     }
 }
